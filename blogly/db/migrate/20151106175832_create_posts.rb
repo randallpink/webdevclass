40a1241c1a7f48ea0ef280f88650b5e3d60fdd8f
Randy Satterfield <Randy.satterfield@careerbuilder.com>
2015-11-06 13:05:10 -0500

    adding blogly

--- blogly/db/migrate/20151106175832_create_posts.rb
+++ blogly/db/migrate/20151106175832_create_posts.rb
@@ -1,8 +1,8 @@
 class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
-      t.string, :name
-      t.text, :content
+      t.string :name
+      t.text :content
       t.string :featured_image
 
       t.timestamps null: false
