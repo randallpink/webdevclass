8845f7ecb2883a8378face107ee4dcf6c64eed92
Randy Satterfield <Randy.satterfield@careerbuilder.com>
2015-11-09 19:59:32 -0500

    updated to add features and some other stuff

--- autotrader/app/controllers/vehicles_controller.rb
+++ autotrader/app/controllers/vehicles_controller.rb
@@ -69,6 +69,10 @@ class VehiclesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def vehicle_params
-      params.require(:vehicle).permit(:category_id, :color, :condition, :price)
+      params.require(:vehicle).permit(:category_id, 
+                                      :color, 
+                                      :condition, 
+                                      :price, 
+                                      {feature_ids: [] })
     end
 end
--- autotrader/db/schema.rb
+++ autotrader/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151109232459) do
+ActiveRecord::Schema.define(version: 20151110001550) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -20,9 +20,23 @@ ActiveRecord::Schema.define(version: 20151109232459) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "features", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
   create_table "makes", force: :cascade do |t|
     t.string   "name"
     t.string   "logo"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "vehicle_features", force: :cascade do |t|
+    t.integer  "vehicle_id"
+    t.integer  "feature_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
